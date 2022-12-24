d29e62e597b0524f87af86ce8b9470269bee8bca
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-24 23:32:12 +0530

    auth slice

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+//get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    message: ''
+}
+
+//Register user
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false
+            state.isError = false
+            state.isSuccess = false
+            state.message = ''
+        }
+    },
+    extraReducers: () => { }
+})
+
+
+export const { reset } = authSlice.actions
+export default authSlice.reducer
