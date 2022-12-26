8e0af62e904240da25bddf1a10ad209e22db3102
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-26 23:31:03 +0530

    store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import goalReducer from '../features/goals/goalSlice'
 
 //create a store
 export const store = configureStore({
     reducer: {
-        authR: authReducer
+        authR: authReducer,
+        goalR: goalReducer
     }
 })
 
