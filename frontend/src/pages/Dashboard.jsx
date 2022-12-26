ff528254c401bdc669757e8fff970ee83605feec
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-26 23:32:15 +0530

    Dashboard component private route

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import { getGoals, reset } from '../features/goals/goalSlice'
+import Spinner from '../components/Spinner';
+
 
 const Dashboard = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.authR)
+    const { goals, isLoading, isSuccess, isError, message } = useSelector((state) => state.goalR)
+
+    useEffect(() => {
+        if (isError) {
+            console.log(message)
+        }
+        if (!user) {
+            navigate('/login')
+        }
+        dispatch(getGoals())
+        return () => {
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch])
+    if (isLoading) {
+        return <Spinner />
+    }
     return (
-        <div>Dashboard</div>
+        <>
+            <section className="heading">
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+            <GoalForm />
+        </>
     )
 }
 
